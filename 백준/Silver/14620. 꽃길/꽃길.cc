0994fbfa289c210528218453da01fada6bbf204f
SinMuSeong <98457202+ms0828@users.noreply.github.com>
2024-07-11 20:29:42 +0900

    [Silver II] Title: 꽃길, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14620. 꽃길/꽃길.cc
+++ 백준/Silver/14620. 꽃길/꽃길.cc
@@ -14,15 +14,18 @@
 #include<cmath>
 #pragma warning(disable:4996)
 using namespace std;
+
 int n, ret = INT_MAX;
 int board[11][11];
 int visited[11][11];
-int dy[5] = { -1, 0, 1, 0, 0 };
-int dx[5] = { 0, 1, 0, -1, 0 };
+int dy[5] = { -1,0,1,0,0 };
+int dx[5] = { 0,1,0,-1,0 };
 
-int flower(int y, int x) {
+int flower(int y, int x)
+{
     int sum = 0;
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 5; i++)
+    {
         int ny = y + dy[i];
         int nx = x + dx[i];
         visited[ny][nx] = 1;
@@ -30,41 +33,51 @@ int flower(int y, int x) {
     }
     return sum;
 }
-
-void cancel(int y, int x) {
-    for (int i = 0; i < 5; i++) {
+void cancel(int y, int x)
+{
+    for (int i = 0; i < 5; i++)
+    {
         int ny = y + dy[i];
         int nx = x + dx[i];
         visited[ny][nx] = 0;
     }
 }
 
-bool isPossible(int y, int x) {
-    for (int i = 0; i < 5; i++) {
+bool isPossible(int y, int x)
+{
+    for (int i = 0; i < 5; i++)
+    {
         int ny = y + dy[i];
         int nx = x + dx[i];
-        if (ny < 1 || ny > n || nx < 1 || nx > n || visited[ny][nx])
+        if (visited[ny][nx])
+            return false;
+        if (ny < 1 || ny > n || nx < 1 || nx > n)
             return false;
     }
     return true;
 }
 
-void go(int cnt, int sum) {
-    if (cnt == 3) {
+void go(int row, int cnt, int sum)
+{
+    if (cnt == 3)
+    {
         ret = min(ret, sum);
         return;
     }
 
-    for (int i = 2; i < n; i++) {
-        for (int j = 2; j < n; j++) {
-            if (isPossible(i, j)) {
-                int price = flower(i, j);
-                go(cnt + 1, sum + price);
+    for (int i = row; i < n; i++)
+    {
+        for (int j = 2; j < n; j++)
+        {
+            if (isPossible(i, j))
+            {
+                go(i, cnt + 1, sum + flower(i,j));
                 cancel(i, j);
             }
         }
     }
 }
+
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -72,13 +85,11 @@ int main() {
     cout.tie(NULL);
 
     cin >> n;
-
+    
     for (int i = 1; i <= n; i++)
         for (int j = 1; j <= n; j++)
             cin >> board[i][j];
 
-    go(0, 0);
-
+    go(2, 0, 0);
     cout << ret << '\n';
-    return 0;
 }
