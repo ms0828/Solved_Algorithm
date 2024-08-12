4264a1ff1dc416899d1e77cf638d6fa245a83aac
SinMuSeong <98457202+ms0828@users.noreply.github.com>
2024-08-12 20:16:47 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include<string>
+#include<vector>
+#include<stack>
+#include<climits>
+#include<map>
+#include<queue>
+#include<set>
+#include<algorithm>
+#include<cstring>
+#include<stdlib.h>
+#include<unordered_set>
+#include<string>
+#include<cmath>
+#pragma warning(disable:4996)
+using namespace std;
+int y;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> y;
+    cout << y - 543 << '\n';
+    
+    return 0;
+}
