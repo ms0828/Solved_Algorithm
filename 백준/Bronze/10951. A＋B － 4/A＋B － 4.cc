e9a62c351872e82cac8e02cdda67c8987f768564
SinMuSeong <98457202+ms0828@users.noreply.github.com>
2024-08-13 16:27:59 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<string>
+#include<vector>
+#include<stack>
+#include<climits>
+#include<map>
+#include<queue>
+#include<set>
+#include<algorithm>
+#include<cstring>
+#include<stdlib.h>
+#include<unordered_set>
+#include<string>
+#include<cmath>
+#pragma warning(disable:4996)
+using namespace std;
+
+int n;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a, b;
+    while (cin >> a >> b) {
+        cout << a + b << '\n';
+    }
+    
+    return 0;
+}
