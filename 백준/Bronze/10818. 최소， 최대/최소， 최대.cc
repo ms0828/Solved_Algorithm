db926abae8582868e1f9fcf7b6f3d1af11f3bb1c
SinMuSeong <98457202+ms0828@users.noreply.github.com>
2024-08-15 17:23:32 +0900

    [Bronze III] Title: 최소, 최대, Time: 92 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include<string>
+#include<vector>
+#include<stack>
+#include<climits>
+#include<map>
+#include<queue>
+#include<set>
+#include<algorithm>
+#include<cstring>
+#include<stdlib.h>
+#include<unordered_set>
+#include<string>
+#include<cmath>
+#pragma warning(disable:4996)
+using namespace std;
+int n, _min = INT_MAX, _max = INT_MIN;
+int a[1000001];
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        _min = min(_min, a[i]);
+        _max = max(_max, a[i]);
+    }
+    cout << _min << " " << _max << '\n';
+    return 0;
+}
