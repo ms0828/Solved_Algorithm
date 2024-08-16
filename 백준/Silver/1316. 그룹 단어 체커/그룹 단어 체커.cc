6b9410b50d123a24ca64d28e61102d1ee35eac14
SinMuSeong <98457202+ms0828@users.noreply.github.com>
2024-08-16 18:14:50 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include<string>
+#include<vector>
+#include<stack>
+#include<climits>
+#include<map>
+#include<queue>
+#include<set>
+#include<algorithm>
+#include<cstring>
+#include<stdlib.h>
+#include<unordered_set>
+#include<sstream>
+#include<cmath>
+#pragma warning(disable:4996)
+using namespace std;
+
+int cnt, n;
+string s[100];
+int a[100][26];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> s[i];
+    
+    for (int i = 0; i < n; i++)
+    {
+        string w = s[i];
+        bool isG = true;
+        char prev = w[0];
+        a[i][w[0] - 'a'] = 1;
+        for (int j = 1; j < w.size(); j++)
+        {
+            if (w[j] != prev)
+            {
+                if (a[i][w[j] - 'a'])
+                {
+                    isG = false;
+                    break;
+                }
+                prev = w[j];
+            }
+            a[i][w[j] - 'a'] = 1;
+        }
+        if (isG)
+            cnt++;
+    }
+    cout << cnt << '\n';
+   
+    return 0;
+}
